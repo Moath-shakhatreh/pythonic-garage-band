234c8712911ece17b56c55a6cf4c20eef5b1e352
Moath-shakhatreh <moath.shakhatreh@gmail.com>
2023-05-10 21:07:49 +0000

    d

--- assets/test_band.py
+++ assets/test_band.py
@@ -41,7 +41,7 @@ def test_drummer_repr():
     assert actual == expected
 
 
-@pytest.mark.skip("todo")
+# @pytest.mark.skip("todo")
 def test_bassist_str():
     meshell = Bassist("Meshell Ndegeocello")
     actual = str(meshell)
@@ -49,7 +49,7 @@ def test_bassist_str():
     assert actual == expected
 
 
-@pytest.mark.skip("todo")
+# @pytest.mark.skip("todo")
 def test_bassist_repr():
     meshell = Bassist("Meshell Ndegeocello")
     actual = repr(meshell)
@@ -57,14 +57,14 @@ def test_bassist_repr():
     assert actual == expected
 
 
-@pytest.mark.skip("todo")
+# @pytest.mark.skip("todo")
 def test_band_name():
     nirvana = Band("Nirvana", [])
 
     assert nirvana.name == "Nirvana"
 
 
-@pytest.mark.skip("todo")
+# @pytest.mark.skip("todo")
 def test_guitarist():
     jimi = Guitarist("Jimi Hendrix")
     assert jimi.name == "Jimi Hendrix"
--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -1,10 +1,14 @@
 
 class Musician :
-    pass
+    
+    @classmethod
+    def get_instrument(cls):
+        return Guitarist.instrument
 
 
-class Guitarist :
-
+class Guitarist(Musician) :
+    
+    instrument = 'guitar'
     def __init__(self,name="Mo'ath"):
         self.name = name
         
@@ -30,17 +34,28 @@ class Drummer :
     def __repr__(self):
        return f'Drummer instance. Name = {self.name}'
 
+
 class Bassist :
-   pass
+    def __init__(self,x="Mo'ath"):
+        self.name = x
+
+    def __str__(self):
+        return f'My name is {self.name} and I play bass'
+
+    def __repr__(self):
+       return f'Bassist instance. Name = {self.name}'
 
 class Band :
-   pass
+   def __init__(self,name,arr):
+       self.name = name
 
 
 
 if __name__ == '__main__':
 
  moath = Guitarist("Mo'ath")
+#  print(str(moath))
 #  print(repr(moath))
 #  print(moath.name)
-#  print(str(moath))
+# print(moath.instrument)
+print(Guitarist.instrument)
